b19d3be6c6920cfeef8674ad75bddfa7342104b1
Miguel Angel Celigueta <maceli@cimne.upc.edu>
2021-06-15 16:05:57 +0200

    name change RayT to Ray-it

--- compile_debug.bat
+++ compile_debug.bat
@@ -3,9 +3,9 @@ set COMPILATION_TYPE=Debug
 
 mkdir build
 
-cmake -DCOMPILATION_TYPE=%COMPILATION_TYPE% . -G "Visual Studio 16 2019" -A x64 -DCMAKE_CXX_FLAGS="/MP3 /EHsc" -B"build\%COMPILATION_TYPE%" 
+cmake -DCOMPILATION_TYPE=%COMPILATION_TYPE% . -G "Visual Studio 16 2019" -A x64 -DCMAKE_CXX_FLAGS="/MP3 /EHsc" -B"build\%COMPILATION_TYPE%"
 
-cmake --build "build\%COMPILATION_TYPE%" --config %COMPILATION_TYPE% 
+cmake --build "build\%COMPILATION_TYPE%" --config %COMPILATION_TYPE%
 
-copy build\%COMPILATION_TYPE%\%COMPILATION_TYPE%\RayT.exe .\RayT_%COMPILATION_TYPE%.exe
+copy build\%COMPILATION_TYPE%\%COMPILATION_TYPE%\Ray-it.exe .\Ray-it_%COMPILATION_TYPE%.exe
 
--- compile_release.bat
+++ compile_release.bat
@@ -3,8 +3,8 @@ set COMPILATION_TYPE=Release
 
 mkdir build
 
-cmake -DCOMPILATION_TYPE=%COMPILATION_TYPE% . -G "Visual Studio 16 2019" -A x64 -DCMAKE_CXX_FLAGS="/MP3 /EHsc /Ot" -B"build\%COMPILATION_TYPE%" 
+cmake -DCOMPILATION_TYPE=%COMPILATION_TYPE% . -G "Visual Studio 16 2019" -A x64 -DCMAKE_CXX_FLAGS="/MP3 /EHsc /Ot" -B"build\%COMPILATION_TYPE%"
 
-cmake --build "build\%COMPILATION_TYPE%" --config %COMPILATION_TYPE% 
+cmake --build "build\%COMPILATION_TYPE%" --config %COMPILATION_TYPE%
 
-copy build\%COMPILATION_TYPE%\%COMPILATION_TYPE%\RayT.exe .\RayT_%COMPILATION_TYPE%.exe
+copy build\%COMPILATION_TYPE%\%COMPILATION_TYPE%\Ray-it.exe .\Ray-it_%COMPILATION_TYPE%.exe
