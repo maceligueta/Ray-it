1615131a6c7c61bd0719e3ac16c2d552a06ebb2a
Miguel Angel Celigueta <maceli@cimne.upc.edu>
2022-07-12 15:26:29 +0200

    dxf parser added

---
+++ external_libraries/DxfLib/Tests/DxfWriterTest.cpp
@@ -0,0 +1,14 @@
+// DxfWriterTest.cpp
+
+// Tests the DxfWriter class
+
+#include "DxfDrawing.hpp"
+#include "TestHelpers.h"
+
+
+
+
+
+IMPLEMENT_TEST_MAIN("DxfWriterTest",
+	fmt::print("TODO\n");
+)
